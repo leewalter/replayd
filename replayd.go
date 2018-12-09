5ca66a8ded2c6f8cf30d3baa450689248dba065b
Walter Lee <quietwalter@gmail.com>
2018-12-09 13:47:44 -0800

    Initial commit

---
+++ replayd.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/exlskills/demo-go-webservice/config"
+	"github.com/exlskills/demo-go-webservice/routes"
+	"github.com/gorilla/handlers"
+)
+
+var Log = config.Cfg().GetLogger()
+
+// This CORS Handler comes with some pretty lenient defaults, depending on your application,
+// you may want to curtail some of these open settings
+var CORSHandler = handlers.CORS(handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}), handlers.AllowCredentials(), handlers.AllowedHeaders([]string{"x-locale", "x-api-key", "content-type", "access-control-request-headers", "access-control-request-method", "x-csrftoken"}), handlers.AllowedOrigins(config.Cfg().AllowedOrigins))
+
+func main() {
+	Log.Info("Setting up database connection ...")
+	/*
+	   for {
+	       err := models.Setup()
+	       if err != nil {
+	           Log.WithError(err).Error("Error setting up database connection, retrying ...")
+	           time.Sleep(time.Second * 3)
+	       } else {
+	           break
+	       }
+	   }
+
+	   Log.Info("Connected to database")
+	*/
+	// TODO: Implement graceful stop
+	Log.Info("Starting HTTP server")
+	http.ListenAndServe(fmt.Sprintf("%s:%s", config.Cfg().ListenAddress, config.Cfg().ListenPort), CORSHandler(handlers.CombinedLoggingHandler(os.Stdout, routes.CreateRouter())))
+	Log.Info("Stopped HTTP server")
+}
